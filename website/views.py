1016e75a29596ed1caf85a29b3383734c6f125be
Farita1 <juandav2003xd@gmail.com>
2026-05-02 15:06:46 -0500

    update

--- website/views.py
+++ website/views.py
@@ -1,9 +1,13 @@
 from flask import Blueprint, render_template
 
+from website.models import Product
+
 
 views = Blueprint('views', __name__)# Esto es como include en Django, es para importar las vistas a la aplicación principal
 
 
 @views.route('/')
 def home():
-    return render_template('home.html')
+    
+    item = Product.query.filter_by(flash_sale=True)
+    return render_template('home.html', item=item)
